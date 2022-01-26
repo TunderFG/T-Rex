f485acc68f3efe6da4213fcbc369d18400f761bb
TunderFG <98181160+TunderFG@users.noreply.github.com>
2022-01-26 14:41:28 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,11 +1,14 @@
 var trex ,trex_correndo;
 var solo ,solo_image;
 var soloinv;
+var nuvem ,nuvem_image;
 
 function preload(){
     trex_correndo = loadAnimation("trex1.png","trex3.png","trex4.png");
     
     solo_image = loadImage("ground2.png");
+
+    nuvem_image = loadImage("cloud2.png");
 }
 
 function setup(){
@@ -19,6 +22,8 @@ function setup(){
     solo.addImage(solo_image);
     solo.x = solo.width/2;
 
+
+
     soloinv = createSprite(300,190,600,10);
     soloinv.visible = false;
 }
@@ -26,7 +31,7 @@ function setup(){
 function draw(){
     background("white");
     solo.velocityX = -2;
-    console.log(frameCount);
+    //console.log(frameCount);
 
     if(solo.x<0){  
         solo.x = solo.width/2;
@@ -36,5 +41,21 @@ function draw(){
 }
     trex.velocityY = trex.velocityY + 0.5;
     trex.collide(soloinv);
+
+    gerarnuvem();
     drawSprites();
+}
+
+function gerarnuvem(){
+    if(frameCount%50 === 0){
+        nuvem = createSprite(600,100,40,10);
+        nuvem.velocityX = -2;
+        nuvem.addImage(nuvem_image);
+        nuvem.y = random(10,100);
+        nuvem.scale = random(0.5,1);
+        console.log(trex.depth);
+        console.log(nuvem.depth);
+        nuvem.depth = trex.depth;
+        trex.depth+=1;
+    }
 }
